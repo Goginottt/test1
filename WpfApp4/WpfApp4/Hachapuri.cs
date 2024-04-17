1b33ab8730559625a4a3df12ef4b01cd3497deb0
Goginottt <125601487+Goginottt@users.noreply.github.com>
2024-04-17 20:46:09 +0300

    Add files via upload

---
+++ WpfApp4/WpfApp4/Hachapuri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp4
+{
+    internal class Hachapuri
+    {
+        public void HD(double c1, double c2)
+        {
+            double c3;
+            c3 = c1 * c2;
+            MessageBox.Show("У вас получилось " + $"{c3}");
+        }
+    }
+}
---
+++ WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Hachapuri GG = new Hachapuri();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void c1_Click(object sender, RoutedEventArgs e)
+        {
+            GG.HD(Convert.ToDouble(x1.Text), Convert.ToDouble(x2.Text));
+        }
+        
+
+
+    }
+
+}
